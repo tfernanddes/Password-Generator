abd30f966c92c1f6c74fa1b8fcc4d94d5b25ebee
Your Name <you@example.com>
2021-12-23 12:53:19 -0300

    Version 1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+## Password Generator
+
+# What is does?
+
+This is a simple script that generate password that are almost impossible to crack by hackers.
+
+# How I did it?
+
+I used "lists", "for" function and "random" module to generate random passwords.
+
+# How can you use it?
+
+You just need to answer three simple questions that the program asks you :)
---
+++ main.py
@@ -0,0 +1,37 @@
+# Password Generator Project
+import random
+
+# Importing list of chars
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Thiago's Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+# we add +1 because if the user types 4 the range will be 1 to 3 only
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+# we need to shuffle the list
+
+random.shuffle(password_list)
+
+# Converting the list to string
+
+password = ""
+for char in password_list:
+    password += char
+print(f"Your password is: {password}")
